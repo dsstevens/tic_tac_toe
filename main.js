e46b72215fc7216c819d155ecb67cf0e97575d60
Deanna Stevens <deanna.sofias@gmail.com>
2023-09-24 16:49:56 -0400

    change id of players within game data model

--- main.js
+++ main.js
@@ -8,13 +8,14 @@ var player2Wins = document.querySelector(".player2Wins")
 window.addEventListener("load", showGamePlay)
 gameBoard.addEventListener("click",function(event){
     event.preventDefault()
+    playSquare(event.target.id,currentPlayer)
 })
 
 // data Model
 var gameData = {
     // hold the player data w the player inside as its own object, the game data, the current state
     player1: {
-        id: 1,
+        id: "player1",
         token: "🐁",
         currentTurn: true,
         wins: 0,
@@ -31,7 +32,7 @@ var gameData = {
         }
     },
     player2: {
-        id: 2,
+        id: "player2",
         token: "🐈‍⬛",
         currentTurn: false,
         wins: 0,
@@ -81,13 +82,13 @@ function showGamePlay(){
     //connect the players in game data to the event listener to initiate game play
     //currently player 1
     // update dom with innerHTML for the first player's turn
-    currentPlayer = gameData.player1
+    currentPlayer = gameData[player1]
     gameState.innerHTML = `It's ${gameData.player1.token}'s Turn!`
 }
 
 //A function that resets the game board’s data to begin a new game
 function resetGame(){
-    playerWinCombos = {
+    player.currentCombos = {
         A: 0,
         B: 0,
         C: 0,
@@ -98,14 +99,15 @@ function resetGame(){
         H: 0
     },
     //reset the turn of the round with a new function ---> alternateRounds() 
-};
+}
 
 function playSquare(squareIdString, player){
     idArray = squareIdString.split("")
     for (var i = 0; i < idArray.length; i++){
-        player[playerTurn][idArray[i]] += 1
+       gameData[player].currentCombos[idArray[i]] += 1
     } //access the player 1 and player 2 nested data within the objects
 }
+
 
 //A function called increaseWins - increases the count of a player’s wins (should work for either player)
 function increaseWins(player){
