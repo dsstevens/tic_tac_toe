f1e053ac34ae789a18e407f42cb9c56edc07b491
Deanna Stevens <deanna.sofias@gmail.com>
2023-09-26 19:47:34 -0400

    Move DOM functions to separate section

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ gameBoard.addEventListener("click",function(event){
     }
 })
 
-// data Model
+// Data Model:
 var gameData = {
     player1: {
         id: "player1",
@@ -103,17 +103,66 @@ function playSquare(squareIdString, player){
     } 
 }
 
-function updateSquareDOM(event,token) {
-    if (!event.target.innerHTML) {
-        event.target.innerHTML += token;
-    }
-}
 
 function switchPlayer(){
     if (currentPlayer.id === 'player1') {
         currentPlayer = gameData['player2']
     } else {
         currentPlayer = gameData['player1']
+    }
+}
+
+
+function increaseWins(player){
+    return player.wins += 1
+}
+
+function checkForWin(player){  
+    var comboArray = Object.keys(gameData[player].currentCombos)
+    for (var i = 0; i < comboArray.length; i++){
+        if(gameData[player].currentCombos[comboArray[i]] > 2){
+            increaseWins(gameData[player])
+            return true
+        }
+    }
+}
+
+function checkForDraw(){
+    if ((gameData.player1.squares.length + gameData.player2.squares.length) === 9) {
+        return true
+    }
+}
+
+function alternateRounds() {
+    gameData.player1.currentTurn = !gameData.player1.currentTurn;
+    gameData.player2.currentTurn = !gameData.player2.currentTurn;
+}
+
+function resetGame(){
+    gameData.player1.currentCombos = { A: 0, B: 0, C: 0, D: 0, E: 0, F: 0, G: 0, H: 0 };
+    gameData.player2.currentCombos = { A: 0, B: 0, C: 0, D: 0, E: 0, F: 0, G: 0, H: 0 };
+    gameData.player1.squares = []
+    gameData.player2.squares = []
+    clearGameBoard()
+    alternateRounds()
+    updateTurnMessage()
+}
+
+function disableSquare(event){
+    event.target.classList.add("disabled")
+}
+
+function enableSquare(){
+    for(var i = 0; i < boxes.length; i++){
+        boxes[i].classList.remove("disabled")
+    }
+    
+}
+
+//DOM FUNCTIONS
+function clearGameBoard(){
+    for(var i = 0; i < boxes.length; i++){
+        boxes[i].innerHTML = ""
     }
 }
 
@@ -135,62 +184,16 @@ function updateDrawMessage(){
     setTimeout(updateTurnMessage, 1000)
 }
 
+function updateSquareDOM(event,token) {
+    if (!event.target.innerHTML) {
+        event.target.innerHTML += token;
+    }
+}
+
 function updateWins(player){
     if(player === gameData.player1){
         player1Wins.innerHTML = `SCORE: ${player.wins}`
     } else {
         player2Wins.innerHTML = `SCORE: ${player.wins}`
     }  
-}
-
-function increaseWins(player){
-    return player.wins += 1
-}
-
-function checkForWin(player){  
-    var comboArray = Object.keys(gameData[player].currentCombos)
-    for (var i = 0; i < comboArray.length; i++){
-        if(gameData[player].currentCombos[comboArray[i]] > 2){
-            increaseWins(gameData[player])
-            return true
-        }
-    }
-}
-
-function checkForDraw(){
-    if ((gameData.player1.squares.length + gameData.player2.squares.length) === 9) {
-       return true
-    }
-}
-
-function alternateRounds() {
-    gameData.player1.currentTurn = !gameData.player1.currentTurn;
-    gameData.player2.currentTurn = !gameData.player2.currentTurn;
-}
-
-function resetGame(){
-    gameData.player1.currentCombos = { A: 0, B: 0, C: 0, D: 0, E: 0, F: 0, G: 0, H: 0 };
-    gameData.player2.currentCombos = { A: 0, B: 0, C: 0, D: 0, E: 0, F: 0, G: 0, H: 0 };
-    gameData.player1.squares = []
-    gameData.player2.squares = []
-    clearGameBoard()
-    alternateRounds()
-    updateTurnMessage()
-}
-
-function clearGameBoard(){
-    for(var i = 0; i < boxes.length; i++){
-        boxes[i].innerHTML = ""
-    }
-}
-
-function disableSquare(event){
-    event.target.classList.add("disabled")
-}
-
-function enableSquare(){
-    for(var i = 0; i < boxes.length; i++){
-        boxes[i].classList.remove("disabled")
-    }
-    
 }
